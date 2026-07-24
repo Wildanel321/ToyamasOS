3e09abd8bd31acba081866dda378336b39f25489
Wildan Nur Awaludin <wildanelgato@gmail.com>
2026-07-24 07:20:23 +0700

    #13

--- toyamas-iso/build-iso.sh
+++ toyamas-iso/build-iso.sh
@@ -141,7 +141,8 @@ lb config \
     --iso-volume "TOYAMASOS_1_0" \
     --security false \
     --apt-indices false \
-    --initsystem systemd
+    --initsystem systemd \
+    --memtest none
 
 # Inject debootstrap options directly to the config file to bypass command-line parsing bugs in Ubuntu's live-build
 echo 'LB_DEBOOTSTRAP_OPTIONS="--include=coreutils,usr-is-merged,systemd --no-check-gpg"' >> config/bootstrap
